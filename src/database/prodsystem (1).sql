30215642e4d4740135b89666c72541abec33ae3a
Seph-Anthony <generalgenx60@gmail.com>
2025-02-28 21:54:46 +0800

    double checking

--- src/database/prodsystem (1).sql
+++ src/database/prodsystem (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 12, 2025 at 09:45 AM
+-- Generation Time: Feb 28, 2025 at 02:51 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,19 +24,51 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `process`
+--
+
+CREATE TABLE `process` (
+  `s_id` int(20) NOT NULL,
+  `u_id` int(50) NOT NULL,
+  `p_id` int(50) NOT NULL,
+  `s_quantity` int(50) NOT NULL,
+  `s_totalam` decimal(10,0) NOT NULL,
+  `s_cash` decimal(10,0) NOT NULL,
+  `s_change` decimal(10,0) NOT NULL,
+  `s_status` varchar(150) NOT NULL,
+  `s_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `p_id` int(10) NOT NULL,
+  `p_name` varchar(20) NOT NULL,
+  `p_price` decimal(10,0) NOT NULL,
+  `p_stock` int(200) NOT NULL,
+  `p_status` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
-  `u_id` int(10) NOT NULL,
-  `u_username` varchar(50) NOT NULL,
-  `u_fname` varchar(50) NOT NULL,
-  `u_lname` varchar(50) NOT NULL,
-  `u_email` varchar(50) NOT NULL,
-  `u_contact` varchar(50) NOT NULL,
-  `u_type` varchar(50) NOT NULL,
-  `u_password` varchar(50) NOT NULL,
-  `u_stat` varchar(50) NOT NULL
+  `u_id` int(20) NOT NULL,
+  `u_username` varchar(150) NOT NULL,
+  `u_fname` varchar(150) NOT NULL,
+  `u_lname` varchar(150) NOT NULL,
+  `u_email` varchar(150) NOT NULL,
+  `u_contact` varchar(150) NOT NULL,
+  `u_type` varchar(150) NOT NULL,
+  `u_password` varchar(150) NOT NULL,
+  `u_stat` varchar(150) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -44,14 +76,25 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`u_id`, `u_username`, `u_fname`, `u_lname`, `u_email`, `u_contact`, `u_type`, `u_password`, `u_stat`) VALUES
-(1, 'joseph', 'Joseph Anthony', 'Arambala', 'joseph@gmail.com', '09878654678', 'Customer', 'josephjoseph', 'Pending'),
-(2, 'heart', 'Heart Immaculate', 'Imalay', 'imalay@gmail.com', '09786556743', 'Customer', 'markjoseph', 'Pending'),
-(3, 'Lloyddyy', 'Lloyd', 'Arambala', 'aram@gmail.com', '09786578654', 'Customer', 'loyydddd', 'Pending'),
-(4, 'josephhh', 'Joseph', 'Arambala', 'anthony@gmail.com', '097864567445', 'Customer', 'josephjoseph', 'Pending');
+(1, 'joseph', 'Joseph Anthony', 'Arambala', 'josephanthony@gmail.com', '09786546785', 'Admin', 'josephanthony', 'Active');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `process`
+--
+ALTER TABLE `process`
+  ADD PRIMARY KEY (`s_id`),
+  ADD KEY `uid` (`u_id`),
+  ADD KEY `pid` (`p_id`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`p_id`);
 
 --
 -- Indexes for table `user`
@@ -64,10 +107,33 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `process`
+--
+ALTER TABLE `process`
+  MODIFY `s_id` int(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `p_id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `u_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `u_id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `process`
+--
+ALTER TABLE `process`
+  ADD CONSTRAINT `pid` FOREIGN KEY (`p_id`) REFERENCES `product` (`p_id`),
+  ADD CONSTRAINT `uid` FOREIGN KEY (`u_id`) REFERENCES `user` (`u_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
