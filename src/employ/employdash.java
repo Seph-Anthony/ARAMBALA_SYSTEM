012347a902c0c4687c9f150d5b3c566d82cc653b
Seph-Anthony <generalgenx60@gmail.com>
2025-02-28 22:55:22 +0800

    checkcheck

--- src/employ/employdash.java
+++ src/employ/employdash.java
@@ -417,6 +417,7 @@ public void displayData(){
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
